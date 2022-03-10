248919501fb54016d68e36ffa1ecf845243aac56
Denilson Novais dos Santos <denilson_n.santos@hotmail.com>
2022-03-09 23:43:33 -0300

    Praticando con java script

---
+++ JavaScript/Exercicios/praticando-JS.js
@@ -0,0 +1,10 @@
+function mostra (frase) {
+    document.write(frase);
+}
+
+function pulaLinha() {
+    document.write("<br>");
+    document.write("<br>");
+}
+
+console.log(mostra('Olá, mundo!'));
